da2d602673328325b0d819d92a5e583f4a77a9ee
sesneha190529 <das05sneha@gmail.com>
2026-08-30 11:24:53 +0530

    added

---
+++ components/signOut-button.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Button } from "./ui/button";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { signOut } from "@/lib/auth-client";
+import { toast } from "sonner";
+
+export default function signOutButton() {
+  const [isPending, setIsPending] = useState(false);
+
+  const router = useRouter();
+
+  async function handleClick() {
+    await signOut({
+      fetchOptions: {
+        onRequest: () => {
+          setIsPending(true);
+        },
+
+        onResponse: () => {
+          setIsPending(false);
+        },
+
+        onError: (ctx) => {
+          toast.error(ctx.error.message);
+        },
+        onSuccess: () => {
+          toast.success("Sign out successfully");
+          router.push("");
+        },
+      },
+    });
+  }
+
+  return (
+    <Button onClick={handleClick} variant="destructive" disabled={isPending}>
+      Sign Out
+    </Button>
+  );
+}

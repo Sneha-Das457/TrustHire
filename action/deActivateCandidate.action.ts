eebf0a1088c9447f890653ef8f46db55e0893d1d
sesneha190529 <das05sneha@gmail.com>
2026-09-18 10:16:26 +0530

    added

---
+++ action/deActivateCandidate.action.ts
@@ -0,0 +1,50 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { APIError } from "better-auth";
+import { headers } from "next/headers";
+
+export default async function deActivateCandidateAction() {
+  try {
+    const session = await auth.api.getSession({
+      headers: await headers(),
+    });
+
+    if (!session?.user?.id) {
+      return { error: "Unauthorized" };
+    }
+
+    const candidateProfile = await prisma.candidate.findUnique({
+      where: {
+        userId: session.user.id,
+      },
+      select: {
+        id: true,
+        isActive: true,
+      },
+    });
+
+    if (!candidateProfile) {
+      return { error: "Cnadidate profile not found" };
+    }
+
+    if (!candidateProfile.isActive) {
+      return { error: "Candidate profile is already deactivated" };
+    }
+
+    await prisma.candidate.update({
+      where: { id: candidateProfile.id },
+      data: {
+        isActive: false,
+      },
+    });
+    return { error: null };
+  } catch (err) {
+    if (err instanceof APIError) {
+      return { error: err.message };
+    }
+
+    return { error: String(err) };
+  }
+}
